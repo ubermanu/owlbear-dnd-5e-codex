c82fecf4ab587af6e9b171ae3190f6aa14ddf177
ubermanu <manu@paca.happy-dev.fr>
2023-12-14 00:21:49 +0100

    clicking on a category renders the results

--- src/Codex.svelte
+++ src/Codex.svelte
@@ -1,48 +1,44 @@
 <script>
   import { debounce } from 'radash'
   import { writable } from 'svelte/store'
+  import { onMount } from 'svelte'
 
   const categories = [
-    { name: 'Spells', emoji: '🔮', path: 'spells' },
-    { name: 'Skills', emoji: '💪', path: 'skills' },
-    { name: 'Equipment', emoji: '⚔️', path: 'equipment' },
-    { name: 'Classes', emoji: '🙋', path: 'classes' },
-    { name: 'Features', emoji: '🎯', path: 'features' },
-    { name: 'Monsters', emoji: '🐉', path: 'monsters' },
+    { name: 'Spells', emoji: '🔮', url: '/api/spells' },
+    { name: 'Skills', emoji: '💪', url: '/api/skills' },
+    { name: 'Equipment', emoji: '⚔️', url: '/api/equipment' },
+    { name: 'Classes', emoji: '🙋', url: '/api/classes' },
+    { name: 'Features', emoji: '🎯', url: '/api/features' },
+    { name: 'Monsters', emoji: '🐉', url: '/api/monsters' },
   ]
 
-  const currentFilter = writable(categories[0])
-
-  let results = []
-  let inputPlaceholder = 'Search...'
-
-  $: {
-    inputPlaceholder = `Search ${$currentFilter.name?.toLowerCase()}...`
-  }
+  const resourceId = writable(categories[0].url)
+  let page = {}
 
   /**
-   * Executes a search against the DnD 5e API
+   * Sets the current category filter
    *
-   * @param {string} query
+   * @param {any} cat
    */
-  async function search(query) {
-    if (query.length < 3) {
-      results = []
-      return
-    }
-
-    const url = `https://www.dnd5eapi.co/api/${$currentFilter.path}/?name=${query}`
-
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      results = data.results
-      console.log(results)
-    } catch (error) {
-      console.error(error)
-      inputPlaceholder = 'Something went wrong...'
-    }
+  async function setCategory(cat) {
+    $resourceId = cat.url
   }
+
+  async function fetchDnD(path) {
+    const url = `https://www.dnd5eapi.co${path}`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    return data
+  }
+
+  async function renderResource() {
+    page = await fetchDnD($resourceId)
+  }
+
+  onMount(async () => {
+    await renderResource()
+  })
 </script>
 
 <main>
@@ -50,28 +46,34 @@
     {#each categories as cat}
       <button
         class="category"
-        on:click={() => ($currentFilter = cat)}
-        class:active={$currentFilter === cat}
+        on:click={() => setCategory(cat)}
+        class:active={$resourceId === cat.url}
       >
         {cat.emoji}
       </button>
     {/each}
   </div>
-  <input
-    class="input"
-    autocomplete="off"
-    type="text"
-    placeholder={inputPlaceholder}
-    on:input={debounce({ delay: 200 }, (e) => search(e.target.value))}
-  />
-  <div class="results">
-    {#each results as result}
-      <div>{result.name}</div>
-    {/each}
+  <!--  <input-->
+  <!--    class="input"-->
+  <!--    autocomplete="off"-->
+  <!--    type="text"-->
+  <!--    placeholder={inputPlaceholder}-->
+  <!--    on:input={debounce({ delay: 200 }, (e) => search(e.target.value))}-->
+  <!--  />-->
+  <div class="content">
+    {#if 'results' in page && Array.isArray(page.results)}
+      <ul class="search-results">
+        {#each page.results as result}
+          <li>{result.name}</li>
+        {/each}
+      </ul>
+    {:else}
+      <pre>{JSON.stringify(page, null, 2)}</pre>
+    {/if}
   </div>
   <footer class="footer">
-    {#if results.length > 0}
-      <div class="count">{results.length} results</div>
+    {#if 'count' in page && +page.count > 0}
+      <div class="count">{page.count} results</div>
     {/if}
   </footer>
 </main>
@@ -123,14 +125,56 @@
     color: #fff;
   }
 
-  .results {
+  .content {
     flex-grow: 1;
+    min-height: 0;
+  }
+
+  .search-results {
+    margin: 0;
+    padding-right: 0.5rem;
+    padding-left: 0;
+    max-height: 100%;
+    overflow-y: auto;
+    list-style: none;
+  }
+
+  .search-results::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+
+  .search-results::-webkit-scrollbar-track {
+    border-radius: 0.2rem;
+    background: #3b3d4b;
+  }
+
+  .search-results::-webkit-scrollbar-thumb {
+    border-radius: 0.2rem;
+    background: #6a6c77;
+  }
+
+  .search-results::-webkit-scrollbar-thumb:hover {
+    background: #bb99ff;
+  }
+
+  .search-results li {
+    transition: background 0.1s ease-out;
+    cursor: pointer;
+    margin-bottom: 0.2rem;
+    border-radius: 0.2rem;
+    padding: 0.5rem;
+    font-size: 1rem;
+  }
+
+  .search-results li:hover {
+    background: #3b3d4b;
   }
 
   .footer {
     display: flex;
     justify-content: space-between;
     align-items: flex-end;
+    margin-top: 1rem;
     color: #6a6c77;
     font-size: 0.8rem;
   }
