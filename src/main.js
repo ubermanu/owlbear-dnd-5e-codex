dc871f966cba4dc8e1d9984126ccec206a1c0905
ubermanu <manu@paca.happy-dev.fr>
2023-12-14 19:23:26 +0100

    reset styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import './app.css'
+import 'the-new-css-reset/css/reset.css'
 import Codex from './Codex.svelte'
+import './app.css'
 
 const app = new Codex({
   target: document.getElementById('app'),
