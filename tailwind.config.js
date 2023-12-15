bddee0553a940d0b8f0fbcdede1257d050c8fbdc
ubermanu <manu@paca.happy-dev.fr>
2023-12-15 17:07:21 +0100

    typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import typography from '@tailwindcss/typography'
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [typography],
 }
