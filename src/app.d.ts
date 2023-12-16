4d051fb26b6fc00cf7bb34ebc9a7ba7336d1e974
ubermanu <manu@paca.happy-dev.fr>
2023-12-16 17:24:55 +0100

    add typedef for OBR in window

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+import OBR from '@owlbear-rodeo/sdk'
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
@@ -7,6 +9,10 @@ declare global {
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
+  }
+
+  interface Window {
+    OBR: typeof OBR
   }
 }
 
