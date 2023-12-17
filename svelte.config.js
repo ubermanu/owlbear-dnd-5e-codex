94df6811776049d4110666d794d7925ca4f670f2
ubermanu <manu@paca.happy-dev.fr>
2023-12-17 13:08:15 +0100

    set base url

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from 'svelte-adapter-ghpages'
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -11,14 +12,14 @@ export default {
       assets: 'build',
       fallback: 'index.html',
     }),
+
+    // Custom base path for GitHub Pages
+    paths: {
+      base: process.env.SVELTEKIT_BASE_URL || '',
+    },
   },
 
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
-  // Custom base path for GitHub Pages
-  paths: {
-    base: process.env.SVELTEKIT_BASE_URL || '',
-  },
 }
