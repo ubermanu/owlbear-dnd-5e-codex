67904ce300be42dff874ac9ad7cc309e07d72e2b
ubermanu <manu@paca.happy-dev.fr>
2023-12-17 16:44:48 +0100

    prerender the category page

--- src/routes/(codex)/api/+layout.js
+++ src/routes/(codex)/api/+layout.js
@@ -1,8 +1,6 @@
 import { PUBLIC_API_URL } from '$env/static/public'
 import { error } from '@sveltejs/kit'
 
-export const ssr = false
-
 export const load = async ({ url, fetch }) => {
   const res = await fetch(PUBLIC_API_URL.concat(url.pathname + url.search))
   const data = await res.json()
